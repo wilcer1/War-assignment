958d899dc8637aa3c6e01b93b7bdeb237ddcb445
Luehunk <lucas.carlsson@outlook.com>
2021-03-02 11:10:58 +0100

    removed card and player from branch
    added test to show_highscore

--- highscore.py
+++ highscore.py
@@ -10,7 +10,8 @@ class Highscore:
         """Set path to highscore_file."""
         self.path = path
 
-    def sort_highscore(self, highscore_list):
+    @classmethod
+    def sort_highscore(cls, highscore_list):
         """Sort highscore_list."""
         sorted_highscore_list = sorted(highscore_list)
         return sorted_highscore_list
@@ -31,8 +32,8 @@ class Highscore:
     def read_highscore(self, path):
         """Read highscore from file and return list."""
         highscore_list = []
-        with open(path, "r") as highscore_file:
-            for line in highscore_file:
+        with open(path, "r") as text_file:
+            for line in text_file:
                 line = line.rstrip("\n")
                 temp = line.split(":")
                 score = int(temp[0])
@@ -42,7 +43,11 @@ class Highscore:
         sorted_highscore_list = self.sort_highscore(highscore_list)
         return sorted_highscore_list
 
-    def show_highscore(self, highscore_list):
+    @classmethod
+    def show_highscore(cls, highscore_list):
         """Show highscore."""
-        for count, (score, player) in enumerate(highscore_list, 1):
-            print(f"{count}. {player} won in {score} rounds.")
+        show_list = []
+        for count, (score, name) in enumerate(highscore_list, 1):
+            show_list.append((f"{count}. {name} won in {score} rounds."))
+
+        return show_list
--- highscore_test.py
+++ highscore_test.py
@@ -9,13 +9,7 @@ class TestHighscoreClass(unittest.TestCase):
 
     def setUp(self):
         """Create test_file.txt file."""
-        self.h = highscore.Highscore("test_file.txt")
-
-        self.unsorted_test_list = []
-        self.unsorted_test_list.append((2, "Joker"))
-        self.unsorted_test_list.append((4, "Batman"))
-        self.unsorted_test_list.append((1, "Bane"))
-        self.unsorted_test_list.append((3, "Robin"))
+        self.highscore = highscore.Highscore("test_file.txt")
 
         self.test_list = []
         self.test_list.append((1, "Bane"))
@@ -23,32 +17,32 @@ class TestHighscoreClass(unittest.TestCase):
         self.test_list.append((3, "Robin"))
         self.test_list.append((4, "Batman"))
 
-    @classmethod
-    def tearDownClass(cls):
-        """Remove test file."""
-        # os.remove("test_file.txt")
-
     def test_init_default_object(self):
         """Test class attribute."""
-        res = self.h.path
-        print(res)
+        res = self.highscore.path
         exp = "test_file.txt"
         self.assertEqual(res, exp)
-        self.assertIsInstance(self.h, highscore.Highscore)
+        self.assertIsInstance(self.highscore, highscore.Highscore)
 
     def test_create_highscore_file(self):
         """Tests if highscore file is created."""
         exp = "test123\n"
-        with open("test_file.txt", "w+") as self.highscore_file:
-            self.highscore_file.write(exp)
-            self.highscore_file.seek(0)
-            res = self.highscore_file.read()
+        with open(self.highscore.path, "w+") as highscore_file:
+            highscore_file.write(exp)
+            highscore_file.seek(0)
+            res = highscore_file.read()
             self.assertEqual(res, exp)
 
     def test_sort_highscore(self):
         """Test if sort method works as intended."""
+        unsorted_test_list = []
+        unsorted_test_list.append((2, "Joker"))
+        unsorted_test_list.append((4, "Batman"))
+        unsorted_test_list.append((1, "Bane"))
+        unsorted_test_list.append((3, "Robin"))
+
         exp = self.test_list
-        res = self.h.sort_highscore(self.unsorted_test_list)
+        res = self.highscore.sort_highscore(unsorted_test_list)
         self.assertListEqual(exp, res)
 
     def test_write_to_highscore(self):
@@ -57,7 +51,7 @@ class TestHighscoreClass(unittest.TestCase):
         for score, name in self.test_list:
             exp.append(f"{score}:{name}\n")
 
-        self.h.write_to_highscore(self.test_list)
+        self.highscore.write_to_highscore(self.test_list)
 
         with open("test_file.txt", "r") as test_file:
             res = []
@@ -70,19 +64,21 @@ class TestHighscoreClass(unittest.TestCase):
 
     def test_add_highscore(self):
         """Test add player highscore to file."""
-        # Remove added player from test_list to not fuck up rest of the test
-        # if run out of order, .
         test_player = player.Player("Gimli")
-        self.h.add_highscore(13, test_player.name)
-        res = self.h.read_highscore(self.h.path)
+        self.highscore.add_highscore(13, test_player.name)
+        res = self.highscore.read_highscore(self.highscore.path)
         exp = self.test_list
         exp.append((13, "Gimli"))
         self.assertEqual(exp, res)
 
     def test_show_highscore(self):
         """Test the output of show_highscore."""
-        # exp = "print directly from test_list?"
-        # res = self.h.show_highscore
+        res = self.highscore.show_highscore(self.test_list)
+        exp = []
+        for count, (score, name) in enumerate(self.test_list, 1):
+            exp.append((f"{count}. {name} won in {score} rounds."))
+
+        self.assertEqual(res, exp)
 
 
 if __name__ == "__main__":
