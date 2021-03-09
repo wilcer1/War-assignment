95ffb66362b4d6f2164fbcf53d81b165ea0daf67
Luehunk <lucas.carlsson@outlook.com>
2021-03-09 13:11:42 +0100

    fixed string is intelligence_test

--- intelligence.py
+++ intelligence.py
@@ -9,7 +9,7 @@ WAR = "This is a war!"
 MATCH_POINT = " has few cards remaining"
 
 
-class Intelligence():
+class Intelligence:
     """Intelligence class."""
 
     def __init__(self):
@@ -25,12 +25,15 @@ class Intelligence():
     def card_1_win(self, card1, card2):
         """Print when card 1 wins."""
         print("------------------------------------------------")
-        print(f"{self.player1.name} wins!\n{card1.show()} beats "
-              + f"{card2.show()}")
-        print(f"{self.player1.name} cards remaining: "
-              + f"{self.player1.cardhand.cards_remaining()}")
-        print(f"{self.player2.name} cards remaining: "
-              + f"{self.player2.cardhand.cards_remaining()}")
+        print(f"{self.player1.name} wins!\n{card1.show()} beats " + f"{card2.show()}")
+        print(
+            f"{self.player1.name} cards remaining: "
+            + f"{self.player1.cardhand.cards_remaining()}"
+        )
+        print(
+            f"{self.player2.name} cards remaining: "
+            + f"{self.player2.cardhand.cards_remaining()}"
+        )
         print("------------------------------------------------")
         if self.player2.name == "Computer":
             print("------------------------------------------------")
@@ -40,12 +43,15 @@ class Intelligence():
     def card_2_win(self, card1, card2):
         """Print when card 2 wins."""
         print("------------------------------------------------")
-        print(f"{self.player2.name} wins!\n{card2.show()} beats "
-              + f"{card1.show()}")
-        print(f"{self.player1.name} cards remaining: "
-              + f"{self.player1.cardhand.cards_remaining()}")
-        print(f"{self.player2.name} cards remaining: "
-              + f"{self.player2.cardhand.cards_remaining()}")
+        print(f"{self.player2.name} wins!\n{card2.show()} beats " + f"{card1.show()}")
+        print(
+            f"{self.player1.name} cards remaining: "
+            + f"{self.player1.cardhand.cards_remaining()}"
+        )
+        print(
+            f"{self.player2.name} cards remaining: "
+            + f"{self.player2.cardhand.cards_remaining()}"
+        )
         print("------------------------------------------------")
         if self.player2.name == "Computer":
             print("------------------------------------------------")
@@ -55,19 +61,15 @@ class Intelligence():
     def war_card_1_win(self, card1, card2, winner_cards):
         """Print when card1 wins war."""
         print("------------------------------------------------")
-        print(f"{self.player1.name} wins!\n{card1.show()} beats "
-              + f"{card2.show()}")
-        print(f"All {len(winner_cards)} cards go to "
-              + f"{self.player1.name}")
+        print(f"{self.player1.name} wins!\n{card1.show()} beats " + f"{card2.show()}")
+        print(f"All {len(winner_cards)} cards go to " + f"{self.player1.name}")
         print("------------------------------------------------")
 
     def war_card_2_win(self, card1, card2, winner_cards):
         """Print when card2 wins war."""
         print("------------------------------------------------")
-        print(f"{self.player2.name} wins!\n{card2.show()} beats "
-              + f"{card1.show()}")
-        print(f"All {len(winner_cards)} cards go to "
-              + f"{self.player2.name}")
+        print(f"{self.player2.name} wins!\n{card2.show()} beats " + f"{card1.show()}")
+        print(f"All {len(winner_cards)} cards go to " + f"{self.player2.name}")
         print("------------------------------------------------")
 
     def war_tie(self, card1, card2):
@@ -88,7 +90,6 @@ class Intelligence():
         """Start function."""
         print(START)
         playsound("start.mp3")
-        
 
     @classmethod
     def war(cls):
