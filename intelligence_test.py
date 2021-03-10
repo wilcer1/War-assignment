cb917431e3feb3ffeb3bb8901d54f7b66eee26dd
wilcer1 <wille.cervin@live.se>
2021-03-10 13:32:21 +0100

    fixed intelligence bug

--- intelligence_test.py
+++ intelligence_test.py
@@ -1,4 +1,4 @@
-"""Test self.intelligence class."""
+"""Test intelligence class."""
 
 import unittest
 import intelligence
@@ -6,34 +6,33 @@ import player
 
 
 class TestIntelligenceClass(unittest.TestCase):
-    """Test self.intelligence class."""
+    """Test intelligence class."""
 
     def setUp(self):
         """Set up."""
-        self.intelligence = intelligence.Intelligence()
         self.player1 = player.Player("Wille")
         self.player2 = player.Player("Lucas")
 
     def test_win(self):
         """Test win function."""
-        self.assertEqual(self.intelligence.WIN, "You are pro!")
+        self.assertEqual(intelligence.WIN, "You are pro!")
 
     def test_lose(self):
         """Test lose function."""
         self.assertEqual(
-            self.intelligence.LOSE,
+            intelligence.LOSE,
             "Better luck next time, human trash. AI will prevail! "
         )
 
     def test_start(self):
         """Test start function."""
-        self.assertEqual(self.intelligence.START, "Good luck have fun!")
+        self.assertEqual(intelligence.START, "Good luck have fun!")
 
     def test_war(self):
         """Test war function."""
-        self.assertEqual(self.intelligence.WAR, "This is a war!")
+        self.assertEqual(intelligence.WAR, "This is a war!")
 
     def test_match_point(self):
         """Test match_point function."""
-        self.assertEqual(self.intelligence.MATCH_POINT,
+        self.assertEqual(intelligence.MATCH_POINT,
                          " has few cards remaining")
