3a569355b9f6f9f260a4297b45c8a80033aa9ff5
TimmyBergvall <timmy.bergvall@hotmail.com>
2021-03-10 14:15:59 +0100

    Fixed a error with coverage

--- intelligence_test.py
+++ intelligence_test.py
@@ -17,8 +17,8 @@ class TestIntelligenceClass(unittest.TestCase):
     def test_lose(self):
         """Test lose function."""
         self.assertEqual(intelligence.LOSE,
-                         "Better luck next time, human trash. ",
-                         "AI will prevail!")
+                         "Better luck next time, human trash. "
+                         + "AI will prevail!")
 
     def test_start(self):
         """Test start function."""
